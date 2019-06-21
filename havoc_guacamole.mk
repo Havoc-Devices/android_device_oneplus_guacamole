af9eda8c23e85bedff2c88f41703189b4b0d8399
Anushek Prasal <anushekprasal@gmail.com>
2019-06-21 10:36:08 +0530

    guacamole: Init Havoc...
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- havoc_guacamole.mk
+++ havoc_guacamole.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 Havoc-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamole
+PRODUCT_NAME := havoc_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := GM1917
@@ -40,3 +41,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+HAVOC_BUILD_TYPE := Official
