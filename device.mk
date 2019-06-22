2f391aff9ca12804d673f08a86c05fedbe590370
Anushek Prasal <anushekprasal@gmail.com>
2019-06-22 16:54:01 +0530

    guacamole: Cleanup overlays
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- device.mk
+++ device.mk
@@ -11,8 +11,7 @@ $(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
